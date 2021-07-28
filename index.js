54913c7787d39eca66736e45738453fcbb30809c
climb719 <mary.oldweiler@gmail.com>
2021-07-28 15:39:59 -0400

    commit tree 810242084e70b668ffa01f1277b568db8a2c393a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thanksArray = []
+    for (let i= 0; i < names.length; i++) {
+    thanksArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+}
+return thanksArray
+}
+
+function countDown(num) {
+   while (num > -1)
+   console.log(num--)
+}
+
